764cd2686fff9d90a75221517e49ba8c700d0185
hutan2401 <tranhuynhminhthuan2401@gmail.com>
2025-03-18 17:50:34 +0700

    cutscene

--- Assets/Scripts/EndingCutscene.cs
+++ Assets/Scripts/EndingCutscene.cs
@@ -6,11 +6,28 @@ public class EndingCutscene : MonoBehaviour
 {
     public GameObject playercam;
     public GameObject cutscenecam;
-    
+    public GameObject player;
+
+    public Animator bedmove;
+    public Animator bedforce;
+    public Animator cameraed;
+
+    public AudioSource endingSound;
 
     private void OnTriggerEnter(Collider other)
     {
-        cutscenecam.SetActive(true);
-        playercam.SetActive(false);
+        if (other.CompareTag("Player"))
+        {
+            cutscenecam.SetActive(true);
+            playercam.SetActive(false);
+            player.GetComponent<MovementController>().enabled = false;
+            endingSound.Play();
+            bedmove.Play("bedmoving");
+            bedforce.Play("force");
+            cameraed.Play("edscene");
+        }
+        
     }
+
+   
 }
