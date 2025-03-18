1812bbc8e4323bd8065d6eb5d636e31a37941797
hutan2401 <tranhuynhminhthuan2401@gmail.com>
2025-03-18 20:04:21 +0700

    update

--- Assets/Scripts/EndingCutscene.cs
+++ Assets/Scripts/EndingCutscene.cs
@@ -1,30 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndingCutscene : MonoBehaviour
 {
-    public GameObject playercam;
-    public GameObject cutscenecam;
-    public GameObject player;
+    //public GameObject playercam;
+    //public GameObject cutscenecam;
+    //public GameObject player;
 
-    public Animator bedmove;
-    public Animator bedforce;
-    public Animator cameraed;
+    //public Animator bedmove;
+    //public Animator bedforce;
+    //public Animator cameraed;
 
-    public AudioSource endingSound;
+    //public AudioSource endingSound;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            cutscenecam.SetActive(true);
-            playercam.SetActive(false);
-            player.GetComponent<MovementController>().enabled = false;
-            endingSound.Play();
-            bedmove.Play("bedmoving");
-            bedforce.Play("force");
-            cameraed.Play("edscene");
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+            //cutscenecam.SetActive(true);
+            //playercam.SetActive(false);
+            //player.GetComponent<MovementController>().enabled = false;
+            //endingSound.Play();
+            //bedmove.Play("bedmoving");
+            //bedforce.Play("force");
+            //cameraed.Play("edscene");
         }
         
     }
